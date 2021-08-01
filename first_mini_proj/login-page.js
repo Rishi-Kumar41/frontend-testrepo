15383558ee32ef12d4a8a62264f2cc6cbedbaa80
Rishi-Kumar41 <rishi.kumar694@gmail.com>
2021-08-02 00:23:26 +0530

    updating codes by ris

--- first_mini_proj/login-page.js
+++ first_mini_proj/login-page.js
@@ -1,8 +1,8 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const loginErrorMsgNoUser = document.getElementById("login-error-msg-no-user");
-const loginErrorMsgNoPassword = document.getElementById("login-error-msg-no-password");
+// const loginErrorMsgNoUser = document.getElementById("login-error-msg-no-user");
+// const loginErrorMsgNoPassword = document.getElementById("login-error-msg-no-password");
 
 
 loginButton.addEventListener("click", (e) => {
@@ -11,28 +11,39 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "ris" && password === "allow") {
-        loginErrorMsgNoUser.style.opacity = 0;
+        // loginErrorMsgNoUser.style.opacity = 0;
         loginErrorMsg.style.opacity = 0;
-        loginErrorMsgNoPassword.style.opacity=0;
-        setTimeout(function(){ alert("You have successfully logged in."); }, 05);
+        // loginErrorMsgNoPassword.style.opacity=0;
        
-        location.reload();
+        alert("You have successfully logged in.");
+        // location.reload();
+        // window.location.href = "Resume.html";
+        setTimeout(window.location.href = "Resume.html", 100);
     }
     else if(username==="") 
     {
-        loginErrorMsgNoUser.style.opacity = 1;
-        loginErrorMsg.style.opacity = 0;
-        loginErrorMsgNoPassword.style.opacity=0;
+        document.getElementById("login-error-msg").innerHTML = "Please enter username";
+        // loginErrorMsgNoUser.style.opacity = 1;
+        loginErrorMsg.style.opacity = 1;
+        // loginErrorMsgNoPassword.style.opacity=0;
     }
     else if(password==="")
     {
-        loginErrorMsgNoUser.style.opacity = 0;
-        loginErrorMsg.style.opacity = 0;
-        loginErrorMsgNoPassword.style.opacity=1;
+        document.getElementById("login-error-msg").innerHTML = "Please enter password";
+        // loginErrorMsgNoUser.style.opacity = 0;
+        loginErrorMsg.style.opacity = 1;
+        // loginErrorMsgNoPassword.style.opacity=1;
     }
     else {
-        loginErrorMsgNoUser.style.opacity = 0;
+        document.getElementById("login-error-msg").innerHTML = "Invalid username and/or password";
+        // loginErrorMsgNoUser.style.opacity = 0;
         loginErrorMsg.style.opacity = 1;
-        loginErrorMsgNoPassword.style.opacity=0;
+        // loginErrorMsgNoPassword.style.opacity=0;
     }
+
 })
+
+function myFunction()
+{
+    loginErrorMsg.style.opacity = 0;
+}
